977cc9fe1d4bbbc3cf962fa8085da3242d8275cd
avytran <tr.anhvy.work@gmail.com>
2025-08-02 21:54:13 +0700

    test 2

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,23 @@
 import express from 'express';
 import userRoutes from './routes/user.route';
 import swaggerUi from 'swagger-ui-express';
-import swaggerJsdoc from 'swagger-jsdoc';
-import swaggerOptions from './swagger/swaggerOptions';
+import YAML from "yaml";
+import fs from "fs";
+import path from "path";
 
 const app = express();
 const port = 8080;
 
-const swaggerSpec = swaggerJsdoc(swaggerOptions);
+// Load YAML (dùng __dirname vì khi deploy Vercel sẽ copy file sang thư mục riêng)
+const filePath = path.join(__dirname, "learn-swagger.yaml");
+const swaggerDocument = YAML.parse(fs.readFileSync(filePath, "utf8"));
 
 app.use(express.json());
 app.use('/api/users', userRoutes);
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.listen(port, () => {
   console.log(`🚀 Server running on http://localhost:${port}`);
 });
+
+export default app;
