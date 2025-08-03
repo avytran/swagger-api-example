66a3ae66f3df1ddf33e61922fba468a90403abe4
avytran <tr.anhvy.work@gmail.com>
2025-08-04 00:37:18 +0700

    test 12

--- src/index.ts
+++ src/index.ts
@@ -4,27 +4,23 @@ import swaggerUi from "swagger-ui-express";
 import YAML from "yaml";
 import fs from "fs";
 import path from "path";
-import swaggerUiDist from "swagger-ui-dist";
 
 const app = express();
 
-// ✅ Đọc file YAML spec
+const CSS_URL =
+  "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css";
+
 const filePath = path.join(process.cwd(), "src/learn-swagger.yaml");
-const swaggerDocument = YAML.parse(fs.readFileSync(filePath, "utf8"));
+const swaggerDocument = YAML.parse(filePath);
 
-// ✅ Serve static file của Swagger UI
-app.use("/api-docs", express.static(swaggerUiDist.getAbsoluteFSPath()));
-
-// ✅ Setup Swagger UI + CSS + fetch spec từ endpoint riêng
 app.use(
   "/api-docs",
   swaggerUi.serve,
-  swaggerUi.setup(null, {
-    swaggerOptions: { url: "/swagger.yaml" },
-    customCssUrl: "https://unpkg.com/swagger-ui-dist/swagger-ui.css", // Dùng bản khớp với JS
+  swaggerUi.setup(swaggerDocument, {
+    customCssUrl: "https://unpkg.com/swagger-ui-dist/swagger-ui.css", 
   })
 );
-// ✅ Endpoint trả YAML spec
+
 app.get("/swagger.yaml", (req, res) => {
   res.sendFile(filePath);
 });
